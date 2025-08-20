7a7e4f3389484af7dd53715e5f737794cf8dd8e3
Harshit-Badgamia <055011@fsm.ac.in>
2025-08-20 10:18:16 +0530

    Update and rename data_cleaning.py to data_loading.py

---
+++ data_loading.py
@@ -0,0 +1,16 @@
+# data_loading.py
+
+import pandas as pd
+import zipfile
+
+# -----------------------------------------
+# Function to extract and load marketing data from a local zip
+# -----------------------------------------
+def load_marketing_data_from_zip(zip_path='marketing_campaign_dataset.zip', csv_filename='marketing_campaign_dataset_cleaned.csv'):
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            with zip_ref.open(csv_filename) as file:
+                df = pd.read_csv(file, encoding='ISO-8859-1', engine='python', on_bad_lines='skip')
+        return df
+    except Exception as e:
+        raise ValueError(f"Error reading file: {e}")
